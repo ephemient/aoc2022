58fc100ef7ee3b8d2495fbf4ce4fab3f2caf0cf9
Daniel Lin <ephemient@gmail.com>
2022-12-01 16:39:31 -0500

    Quickselect instead of full sort

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day1.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day1.kt
@@ -17,5 +17,36 @@ class Day1(lines: List<String>) {
 
     fun part1(): Int = sums.max()
 
-    fun part2(): Int = sums.sorted().takeLast(3).sum()
+    fun part2(): Int {
+        val sums = sums.toIntArray()
+        sums.quickSelectDescending(2)
+        return sums[0] + sums[1] + sums[2]
+    }
+}
+
+private fun IntArray.quickSelectDescending(index: Int) {
+    require(index in indices)
+    var start = 0
+    var end = size
+    var iter = 0
+    while (end - start > 1) {
+        require(iter++ < 120)
+        val pivotIndex = start + (end - start) / 2
+        val pivot = this[pivotIndex]
+        this[pivotIndex] = this[end - 1]
+        var n = start
+        for (i in start until end - 1) {
+            val tmp = this[i]
+            if (tmp < pivot) continue
+            this[i] = this[n]
+            this[n++] = tmp
+        }
+        this[end - 1] = this[n]
+        this[n] = pivot
+        if (n < index) {
+            start = n + 1
+        } else {
+            end = n
+        }
+    }
 }
