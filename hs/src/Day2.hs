0349f218a3f34086851396d8ac793a03d39a8a3f
Daniel Lin <ephemient@gmail.com>
2022-12-02 00:45:57 -0500

    Day 2: Rock Paper Scissors

---
+++ hs/src/Day2.hs
@@ -0,0 +1,24 @@
+{-|
+Module:         Day2
+Description:    <https://adventofcode.com/2022/day/2 Day 2: Rock Paper Scissors>
+-}
+{-# LANGUAGE DataKinds, OverloadedStrings #-}
+module Day2 (day2a, day2b) where
+
+import Data.Char (ord)
+import Data.Text (Text)
+import qualified Data.Text as T (head, lines, words)
+
+parse :: Text -> (Int, Int)
+parse line = (parse1 lhs, parse1 rhs) where
+    [lhs, rhs] = T.words line
+    parse1 = succ . (`mod` 3) . (`mod` 23) . pred . ord . T.head
+
+score :: Int -> Int -> Int
+score lhs rhs = (1 + rhs - lhs) `mod` 3 * 3 + rhs
+
+day2a :: Text -> Int
+day2a input = sum $ uncurry score . parse <$> T.lines input
+
+day2b :: Text -> Int
+day2b input = sum [score lhs $ 1 + (lhs + rhs) `mod` 3 | (lhs, rhs) <- parse <$> T.lines input]
---
+++ hs/test/Day2Spec.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Day2Spec (spec) where
+
+import Data.Text (Text)
+import qualified Data.Text as T (unlines)
+import Day2 (day2a, day2b)
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+example :: Text
+example = T.unlines
+  [ "A Y"
+  , "B X"
+  , "C Z"
+  ]
+
+spec :: Spec
+spec = do
+    describe "part 2" $ do
+        it "examples" $ do
+            day2a example `shouldBe` 15
+    describe "part 3" $ do
+        it "examples" $ do
+            day2b example `shouldBe` 12
