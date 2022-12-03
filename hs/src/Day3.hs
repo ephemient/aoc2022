64ca75b72bf7941bd3164a29a8932e972b7e95be
Daniel Lin <ephemient@gmail.com>
2022-12-03 02:31:52 -0500

    Use Word64 as a bitset instead of IntSet

--- hs/src/Day3.hs
+++ hs/src/Day3.hs
@@ -4,25 +4,28 @@ Description:    <https://adventofcode.com/2022/day/3 Day 3: Rucksack Reorganizat
 -}
 module Day3 (day3a, day3b) where
 
+import Data.Bits (Bits((.&.), setBit, testBit, zeroBits), FiniteBits(finiteBitSize))
 import Data.Char (ord)
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IntSet (fromList, intersection, toList)
 import Data.List (foldl1')
 import Data.List.Split (chunksOf)
 import Data.Text (Text)
-import qualified Data.Text as T (length, lines, splitAt, unpack)
+import Data.Word (Word64)
+import qualified Data.Text as T (foldl', length, lines, splitAt)
 
-items :: Text -> IntSet
-items = IntSet.fromList . map prio . T.unpack where
+items :: Text -> Word64
+items = T.foldl' (\acc c -> setBit acc $ prio c) zeroBits where
     prio c = if c >= 'a' then ord c - ord 'a' + 1 else ord c - ord 'A' + 27
+
+bits :: (FiniteBits b) => b -> [Int]
+bits x = filter (testBit x) [0 .. finiteBitSize x - 1]
 
 day3a :: Text -> Int
 day3a input = sum $ do
     line <- T.lines input
     let (a, b) = T.splitAt (T.length line `div` 2) line
-    IntSet.toList $ items a `IntSet.intersection` items b
+    bits $ items a .&. items b
 
 day3b :: Text -> Int
 day3b input = sum $ do
     group <- chunksOf 3 $ T.lines input
-    IntSet.toList . foldl1' IntSet.intersection $ items <$> group
+    bits . foldl1' (.&.) $ items <$> group
