a0a6fc1a9cc4c032b74c2c9258aec0daf549a474
Daniel Lin <ephemient@gmail.com>
2022-12-03 02:42:03 -0500

    Use Long as a bitset instead of Set<Int>

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day3.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day3.kt
@@ -2,19 +2,34 @@ package com.github.ephemient.aoc2022
 
 class Day3(private val lines: List<String>) {
     fun part1(): Int = lines.sumOf {
-        (it.items(end = it.length / 2) intersect it.items(start = it.length / 2)).sum()
+        (it.items(end = it.length / 2) and it.items(start = it.length / 2)).bitSum()
     }
 
     fun part2(): Int = lines.chunked(3).sumOf { (a, b, c) ->
-        (a.items() intersect b.items() intersect c.items()).single()
+        (a.items() and b.items() and c.items()).singleBit()
     }
 }
 
-private fun String.items(start: Int = 0, end: Int = length): Set<Int> = buildSet {
+private fun String.items(start: Int = 0, end: Int = length): Long {
+    var set = 0L
     for (i in start until end) {
         when (val c = get(i)) {
-            in 'a'..'z' -> add(c - 'a' + 1)
-            in 'A'..'Z' -> add(c - 'A' + 27)
+            in 'a'..'z' -> set = set or 1L.shl(c - 'a' + 1)
+            in 'A'..'Z' -> set = set or 1L.shl(c - 'A' + 27)
         }
     }
+    return set
+}
+
+private fun Long.bitSum(): Int {
+    var sum = 0
+    for (i in 0 until 64) {
+        if (this and 1L.shl(i) != 0L) sum += i
+    }
+    return sum
+}
+
+private fun Long.singleBit(): Int {
+    require(this != 0L && this and this - 1 == 0L)
+    return countTrailingZeroBits()
 }
