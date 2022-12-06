ac9f807a562b769ab4a40dc0d59b34f471ffb568
Daniel Lin <ephemient@gmail.com>
2022-12-06 04:51:59 -0500

    Handle short reads and use line-sized buffer instead of file-sized

--- kt/src/nativeMain/kotlin/com/github/ephemient/aoc2022/Main.kt
+++ kt/src/nativeMain/kotlin/com/github/ephemient/aoc2022/Main.kt
@@ -6,40 +6,46 @@ import platform.posix.*
 @OptIn(UnsafeNumber::class)
 actual fun getInput(day: Int): List<String> {
     val dataDir = checkNotNull(getenv("aoc2022_data")) { "missing \$aoc2022_data" }.toKString()
+    var offset = 0
+    var buffer = ByteArray(2 * BUFSIZ)
     val fd = open("$dataDir/day$day.txt", O_RDONLY)
     check(fd != -1) {
         val errno = errno
         "open(\$aoc2022_data/day$day.txt): ${strerror(errno)?.toKString() ?: errno.toString()}"
     }
-    var length = 0
-    var buffer: ByteArray
     try {
-        buffer = ByteArray(cValue<stat> { fstat(fd, ptr) }.size.coerceAtLeast(0) + BUFSIZ)
-        do {
-            val readSize = (buffer.size - length).coerceAtLeast(BUFSIZ)
-            val count = buffer.usePinned { read(fd, it.addressOf(length), readSize.convert()) }
-            check(count >= 0) {
-                val errno = errno
-                "read(\$aoc2022_data/day$day.txt): ${strerror(errno)?.toKString() ?: errno.toString()}"
+        return buildList {
+            while (true) {
+                val count = buffer.usePinned {
+                    read(fd, it.addressOf(offset), (buffer.size - offset).coerceAtMost(BUFSIZ).convert())
+                }
+                when {
+                    count < 0 -> {
+                        val errno = errno
+                        check(errno == EINTR) {
+                            "read(\$aoc2022_data/day$day.txt): ${strerror(errno)?.toKString() ?: errno.toString()}"
+                        }
+                        continue
+                    }
+                    count > 0 -> {
+                        var start = 0
+                        val end = offset + count.toInt()
+                        for (pos in offset until end) {
+                            if (buffer[pos] == '\n'.code.toByte()) {
+                                add(buffer.toKString(startIndex = start, endIndex = pos))
+                                start = pos + 1
+                            }
+                        }
+                        buffer.copyInto(buffer, startIndex = start, endIndex = end)
+                        offset = end - start
+                        if (buffer.size - offset < BUFSIZ) buffer = buffer.copyOf(buffer.size * 2)
+                    }
+                    else -> break
+                }
             }
-            length += count.toInt()
-            if (length + BUFSIZ > buffer.size) {
-                val newSize = maxOf(length + BUFSIZ, Int.MIN_VALUE ushr buffer.size.countLeadingZeroBits() - 1)
-                buffer = buffer.copyOf(newSize)
-            }
-        } while (count >= readSize)
+            if (offset != 0) add(buffer.toKString(endIndex = offset))
+        }
     } finally {
         close(fd)
-    }
-    return buildList {
-        var i = 0
-        while (i < length) {
-            var j = i
-            while (buffer[j] != '\n'.code.toByte()) {
-                if (++j >= length) break
-            }
-            add(buffer.toKString(i, j))
-            i = j + 1
-        }
     }
 }
