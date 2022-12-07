29fd338bdfd89032f58086d1924992d88e653a3a
Daniel Lin <ephemient@gmail.com>
2022-12-07 06:07:35 -0500

    Strict is faster

--- hs/src/Day7.hs
+++ hs/src/Day7.hs
@@ -6,8 +6,8 @@ Description:    <https://adventofcode.com/2022/day/7 Day 7: No Space Left On Dev
 module Day7 (day7a, day7b) where
 
 import Data.List (find, foldl', sort, tails)
-import Data.Map (Map)
-import qualified Data.Map as Map ((!), elems, insertWith, singleton)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map ((!), elems, insertWith, singleton)
 import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text as T (lines, stripPrefix)
@@ -16,12 +16,11 @@ import qualified Data.Text.Read as T (decimal)
 parse :: (Integral a) => Text -> Map [Text] a
 parse = snd . foldl' go ([], Map.singleton [] 0) . T.lines where
     go (_, dirs) "$ cd /" = ([], dirs)
-    go (_:cwd, dirs) "$ cd .." = (cwd, dirs)
+    go (cwd, dirs) "$ cd .." = (drop 1 cwd, dirs)
     go (cwd, dirs) (T.stripPrefix "$ cd " -> Just dir) = (dir:cwd, dirs)
-    go (cwd, dirs) (T.stripPrefix "dir " -> Just dir) = (cwd, Map.insertWith (const id) (dir:cwd) 0 dirs)
-    go (cwd, dirs) (T.decimal -> Right (size, _)) =
-        (cwd, foldl' (flip $ Map.insertWith (+) `flip` size) dirs $ tails cwd)
+    go (cwd, dirs) (T.decimal -> Right (size, _)) = (cwd, foldl' (f size) dirs $ tails cwd)
     go state _ = state
+    f !size dirs dir = Map.insertWith (+) dir size dirs
 
 day7a :: Text -> Int
 day7a = sum . filter (<= 100000) . Map.elems . parse
