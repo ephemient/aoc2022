6903eed0045d8094bca2d8dc2e53a8c964d4a7d7
Daniel Lin <ephemient@gmail.com>
2022-12-10 03:26:45 -0500

    Day 10: Cathode-Ray Tube

---
+++ py/aoc2022/day10.py
@@ -0,0 +1,206 @@
+"""
+Day 10: Cathode-Ray Tube
+"""
+
+SAMPLE_INPUT = [
+    "addx 15",
+    "addx -11",
+    "addx 6",
+    "addx -3",
+    "addx 5",
+    "addx -1",
+    "addx -8",
+    "addx 13",
+    "addx 4",
+    "noop",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx -35",
+    "addx 1",
+    "addx 24",
+    "addx -19",
+    "addx 1",
+    "addx 16",
+    "addx -11",
+    "noop",
+    "noop",
+    "addx 21",
+    "addx -15",
+    "noop",
+    "noop",
+    "addx -3",
+    "addx 9",
+    "addx 1",
+    "addx -3",
+    "addx 8",
+    "addx 1",
+    "addx 5",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "addx -36",
+    "noop",
+    "addx 1",
+    "addx 7",
+    "noop",
+    "noop",
+    "noop",
+    "addx 2",
+    "addx 6",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "addx 1",
+    "noop",
+    "noop",
+    "addx 7",
+    "addx 1",
+    "noop",
+    "addx -13",
+    "addx 13",
+    "addx 7",
+    "noop",
+    "addx 1",
+    "addx -33",
+    "noop",
+    "noop",
+    "noop",
+    "addx 2",
+    "noop",
+    "noop",
+    "noop",
+    "addx 8",
+    "noop",
+    "addx -1",
+    "addx 2",
+    "addx 1",
+    "noop",
+    "addx 17",
+    "addx -9",
+    "addx 1",
+    "addx 1",
+    "addx -3",
+    "addx 11",
+    "noop",
+    "noop",
+    "addx 1",
+    "noop",
+    "addx 1",
+    "noop",
+    "noop",
+    "addx -13",
+    "addx -19",
+    "addx 1",
+    "addx 3",
+    "addx 26",
+    "addx -30",
+    "addx 12",
+    "addx -1",
+    "addx 3",
+    "addx 1",
+    "noop",
+    "noop",
+    "noop",
+    "addx -9",
+    "addx 18",
+    "addx 1",
+    "addx 2",
+    "noop",
+    "noop",
+    "addx 9",
+    "noop",
+    "noop",
+    "noop",
+    "addx -1",
+    "addx 2",
+    "addx -37",
+    "addx 1",
+    "addx 3",
+    "noop",
+    "addx 15",
+    "addx -21",
+    "addx 22",
+    "addx -6",
+    "addx 1",
+    "noop",
+    "addx 2",
+    "addx 1",
+    "noop",
+    "addx -10",
+    "noop",
+    "noop",
+    "addx 20",
+    "addx 1",
+    "addx 2",
+    "addx 2",
+    "addx -6",
+    "addx -11",
+    "noop",
+    "noop",
+    "noop",
+]
+
+
+def _signals(lines, cycle=0, register=1):
+    for line in lines:
+        if line.startswith("addx "):
+            cycle += 2
+            register += int(line[5:])
+        else:
+            cycle += 1
+        yield cycle, register
+
+
+def part1(lines):
+    """
+    >>> part1(SAMPLE_INPUT)
+    13140
+    """
+    cycle, register = 0, 1
+    signals = _signals(lines)
+    result = 0
+    for i in (20, 60, 100, 140, 180, 220):
+        last_register = register
+        while cycle < i:
+            last_register = register
+            cycle, register = next(signals)
+        result += i * last_register
+    return result
+
+
+def part2(lines):
+    """
+    >>> print(part2(SAMPLE_INPUT))
+    ▓▓░░▓▓░░▓▓░░▓▓░░▓▓░░▓▓░░▓▓░░▓▓░░▓▓░░▓▓░░
+    ▓▓▓░░░▓▓▓░░░▓▓▓░░░▓▓▓░░░▓▓▓░░░▓▓▓░░░▓▓▓░
+    ▓▓▓▓░░░░▓▓▓▓░░░░▓▓▓▓░░░░▓▓▓▓░░░░▓▓▓▓░░░░
+    ▓▓▓▓▓░░░░░▓▓▓▓▓░░░░░▓▓▓▓▓░░░░░▓▓▓▓▓░░░░░
+    ▓▓▓▓▓▓░░░░░░▓▓▓▓▓▓░░░░░░▓▓▓▓▓▓░░░░░░▓▓▓▓
+    ▓▓▓▓▓▓▓░░░░░░░▓▓▓▓▓▓▓░░░░░░░▓▓▓▓▓▓▓░░░░░
+    """
+    cycle, register, last_register = 0, 1, 1
+    signals = _signals(lines)
+    result = []
+    for row in range(6):
+        line = ""
+        for col in range(40):
+            while cycle <= 40 * row + col:
+                last_register = register
+                cycle, register = next(signals)
+            line += "\u2593" if -1 <= last_register - col <= 1 else "\u2591"
+        result.append(line)
+    return "\n".join(result)
+
+
+parts = (part1, part2)
