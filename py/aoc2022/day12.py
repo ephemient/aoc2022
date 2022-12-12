7149dd7c0439f47c993ae0fbe2d5cb96efba7222
Daniel Lin <ephemient@gmail.com>
2022-12-12 02:49:00 -0500

    Formatting

--- py/aoc2022/day12.py
+++ py/aoc2022/day12.py
@@ -1,8 +1,10 @@
 """
 Day 12: Hill Climbing Algorithm
 """
+# pylint: disable=invalid-name
 
 from collections import deque
+
 
 def _bfs(start, neighbors):
     seen = {start}
@@ -22,7 +24,9 @@ def part1(lines):
     >>> part1(["Sabqponm", "abcryxxl", "accszExk", "acctuvwj", "abdefghi"])
     31
     """
-    start, = ((y, x) for y, line in enumerate(lines) for x, c in enumerate(line) if c == 'S')
+    (start,) = (
+        (y, x) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "S"
+    )
 
     def neighbors(point):
         y0, x0 = point
@@ -34,11 +38,17 @@ def part1(lines):
                 if x1 not in range(len(lines[y1])):
                     continue
                 b = lines[y1][x1]
-                if b == 'a' if a == 'S' else a == 'z' if b == 'E' else ord(b) - ord(a) <= 1:
+                if (
+                    b == "a"
+                    if a == "S"
+                    else a == "z"
+                    if b == "E"
+                    else ord(b) - ord(a) <= 1
+                ):
                     yield y1, x1
 
     for (y, x), depth in _bfs(start, neighbors):
-        if lines[y][x] == 'E':
+        if lines[y][x] == "E":
             return depth
     return None
 
@@ -48,7 +58,9 @@ def part2(lines):
     >>> part2(["Sabqponm", "abcryxxl", "accszExk", "acctuvwj", "abdefghi"])
     29
     """
-    start, = ((y, x) for y, line in enumerate(lines) for x, c in enumerate(line) if c == 'E')
+    (start,) = (
+        (y, x) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "E"
+    )
 
     def neighbors(point):
         y0, x0 = point
@@ -60,11 +72,11 @@ def part2(lines):
                 if x1 not in range(len(lines[y1])):
                     continue
                 b = lines[y1][x1]
-                if b == 'z' if a == 'E' else ord(a) - ord(b) <= 1:
+                if b == "z" if a == "E" else ord(a) - ord(b) <= 1:
                     yield y1, x1
 
     for (y, x), depth in _bfs(start, neighbors):
-        if lines[y][x] == 'a':
+        if lines[y][x] == "a":
             return depth
     return None
 
