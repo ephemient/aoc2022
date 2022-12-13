8efef873051f64bdd1095dcaa7618e60af04a0f0
Daniel Lin <ephemient@gmail.com>
2022-12-13 01:27:17 -0500

    Day 13: Distress Signal

---
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day13.kt
@@ -0,0 +1,80 @@
+package com.github.ephemient.aoc2022
+
+@Day
+class Day13(lines: List<String>) {
+    private val packets = lines.mapNotNull { if (it.isNotEmpty()) it.toPacket() else null }
+
+    @Day.Part
+    fun part1(): Int = packets.chunked(2) { (a, b) -> a <= b }.withIndex().filter { it.value }.sumOf { it.index + 1}
+
+    @Day.Part
+    fun part2(): Int {
+        var x = 1
+        var y = 1
+        for (packet in packets) {
+            if (packet < a) x++
+            else if (packet < b) y++
+        }
+        return x * (x + y)
+    }
+
+    private sealed class Packet : Comparable<Packet> {
+        data class Literal(val value: Int) : Packet() {
+            override fun compareTo(other: Packet): Int = when (other) {
+                is Literal -> this.value compareTo other.value
+                is List -> List(listOf(this)) compareTo other
+            }
+            override fun hashCode(): Int = value
+        }
+
+        data class List(val list: kotlin.collections.List<Packet>) : Packet() {
+            override fun compareTo(other: Packet): Int {
+                return when (other) {
+                    is Literal -> this compareTo List(listOf(other))
+                    is List -> {
+                        val i = this.list.iterator()
+                        val j = other.list.iterator()
+                        while (i.hasNext() && j.hasNext()) {
+                            val comparison = i.next() compareTo j.next()
+                            if (comparison != 0) return comparison
+                        }
+                        i.hasNext() compareTo j.hasNext()
+                    }
+                }
+            }
+            override fun hashCode(): Int = list.fold(0) { acc, value -> 31 * acc + value.hashCode() }
+        }
+
+        override fun equals(other: Any?): Boolean = other is Packet && compareTo(other) == 0
+        abstract override fun hashCode(): Int
+    }
+
+    companion object {
+        private val a: Packet = Packet.List(listOf(Packet.List(listOf(Packet.Literal(2)))))
+        private val b: Packet = Packet.List(listOf(Packet.List(listOf(Packet.Literal(6)))))
+
+        private val parser = DeepRecursiveFunction<IndexedValue<String>, IndexedValue<Packet>> { (startIndex, string) ->
+            if (string[startIndex] == '[') {
+                var index = startIndex + 1
+                val list = buildList {
+                    while (string[index] != ']') {
+                        val (endIndex, value) = callRecursive(IndexedValue(index, string))
+                        add(value)
+                        index = if (string[endIndex] == ',') endIndex + 1 else endIndex
+                    }
+                }
+                IndexedValue(index + 1, Packet.List(list))
+            } else {
+                var index = startIndex + 1
+                while (index < string.length && string[index] in '0'..'9') index++
+                IndexedValue(index, Packet.Literal(string.substring(startIndex, index).toInt()))
+            }
+        }
+
+        private fun String.toPacket(): Packet {
+            val (index, packet) = parser.invoke(IndexedValue(0, this))
+            require(index == length)
+            return packet
+        }
+    }
+}
---
+++ kt/src/commonTest/kotlin/com/github/ephemient/aoc2022/Day13Test.kt
@@ -0,0 +1,44 @@
+package com.github.ephemient.aoc2022
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day13Test {
+    @Test
+    fun part1() {
+        assertEquals(13, Day13(SAMPLE_INPUT).part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(140, Day13(SAMPLE_INPUT).part2())
+    }
+
+    companion object {
+        private val SAMPLE_INPUT = listOf(
+            "[1,1,3,1,1]",
+            "[1,1,5,1,1]",
+            "",
+            "[[1],[2,3,4]]",
+            "[[1],4]",
+            "",
+            "[9]",
+            "[[8,7,6]]",
+            "",
+            "[[4,4],4,4]",
+            "[[4,4],4,4,4]",
+            "",
+            "[7,7,7,7]",
+            "[7,7,7]",
+            "",
+            "[]",
+            "[3]",
+            "",
+            "[[[]]]",
+            "[[]]",
+            "",
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]"
+        )
+    }
+}
