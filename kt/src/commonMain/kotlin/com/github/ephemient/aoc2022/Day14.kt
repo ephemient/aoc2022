bdb8dd75237aac0df5d1378a444534f8c853456a
Daniel Lin <ephemient@gmail.com>
2022-12-14 04:21:45 -0500

    Speed up with bitmap instead of set

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day14.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day14.kt
@@ -2,7 +2,22 @@ package com.github.ephemient.aoc2022
 
 @Day
 class Day14(lines: List<String>) {
-    private val blocks = buildSet {
+    private val blocks: Bitmap
+    private val maxY: Int
+
+    init {
+        var maxX = 0
+        var maxY = 0
+        for (line in lines) {
+            for (point in line.splitToSequence(" -> ")) {
+                val (x, y) = point.split(',', limit = 2)
+                maxX = maxOf(maxX, x.toInt())
+                maxY = maxOf(maxY, y.toInt())
+            }
+        }
+        this.maxY = maxY
+        val width = maxOf(maxX + 1, 500 + maxY + 2)
+        blocks = Bitmap(width, maxY + 2)
         for (line in lines) {
             line.splitToSequence(" -> ").map {
                 val (x, y) = it.split(",", limit = 2)
@@ -10,36 +25,57 @@ class Day14(lines: List<String>) {
             }.zipWithNext { (x1, y1), (x2, y2) ->
                 for (x in minOf(x1, x2)..maxOf(x1, x2)) {
                     for (y in minOf(y1, y2)..maxOf(y1, y2)) {
-                        add(x to y)
+                        blocks[x, y] = true
                     }
                 }
             }.count()
         }
     }
-    private val maxY = blocks.maxOf { it.second }
 
     @Day.Part
     fun part1(): Int {
-        val blocks = blocks.toMutableSet()
-        return generateSequence { fall(blocks, maxY) }.takeWhile { it.second < maxY }.onEach { blocks.add(it) }.count()
+        val blocks = Bitmap(blocks)
+        return generateSequence { fall(blocks, maxY) }
+            .takeWhile { it.second < maxY }
+            .onEach { blocks[it.first, it.second] = true }
+            .count()
     }
 
     @Day.Part
     fun part2(): Int {
-        val blocks = blocks.toMutableSet()
-        return generateSequence { fall(blocks, maxY + 1) }.onEach { blocks.add(it) }.indexOf(500 to 0) + 1
+        val blocks = Bitmap(blocks)
+        return generateSequence { fall(blocks, maxY + 1) }
+            .onEach { blocks[it.first, it.second] = true }
+            .indexOf(500 to 0) + 1
     }
-}
 
-private fun fall(blocks: Set<IntPair>, maxY: Int): IntPair {
-    var x = 500
-    for (y in 1..maxY) {
-        when {
-            x to y !in blocks -> {}
-            x - 1 to y !in blocks -> x--
-            x + 1 to y !in blocks -> x++
-            else -> return x to y - 1
+    private class Bitmap private constructor(private val data: BooleanArray, private val width: Int) {
+        constructor(width: Int, height: Int) : this(BooleanArray(width * height), width)
+        constructor(other: Bitmap) : this(other.data.copyOf(), other.width)
+
+        operator fun get(x: Int, y: Int): Boolean {
+            require(x in 0 until width)
+            return data[x + y * width]
+        }
+
+        operator fun set(x: Int, y: Int, value: Boolean) {
+            require(x in 0 until width)
+            data[x + y * width] = value
         }
     }
-    return x to maxY
+
+    companion object {
+        private fun fall(blocks: Bitmap, maxY: Int): IntPair {
+            var x = 500
+            for (y in 1..maxY) {
+                when {
+                    !blocks[x, y] -> {}
+                    !blocks[x - 1, y] -> x--
+                    !blocks[x + 1, y] -> x++
+                    else -> return x to y - 1
+                }
+            }
+            return x to maxY
+        }
+    }
 }
