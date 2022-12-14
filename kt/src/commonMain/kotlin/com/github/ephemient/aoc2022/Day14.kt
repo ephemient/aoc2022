a046b027ce6d9799a710d30d798c1d8793ed2490
Daniel Lin <ephemient@gmail.com>
2022-12-14 10:30:35 -0500

    Change to recursive implementation and find part 1 on the way to part 2

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day14.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day14.kt
@@ -2,80 +2,52 @@ package com.github.ephemient.aoc2022
 
 @Day
 class Day14(lines: List<String>) {
-    private val blocks: Bitmap
-    private val maxY: Int
-
-    init {
+    private val result = run {
         var maxX = 0
         var maxY = 0
-        for (line in lines) {
-            for (point in line.splitToSequence(" -> ")) {
-                val (x, y) = point.split(',', limit = 2)
-                maxX = maxOf(maxX, x.toInt())
-                maxY = maxOf(maxY, y.toInt())
+        val points = lines.map { line ->
+            line.split(" -> ").map {
+                val (x, y) = it.split(',', limit = 2)
+                x.toInt() to y.toInt()
+            }.onEach { (x, y) ->
+                check(x >= 0 && y >= 0)
+                if (x > maxX) maxX = x
+                if (y > maxY) maxY = y
             }
         }
-        this.maxY = maxY
-        val width = maxOf(maxX + 1, 500 + maxY + 2)
-        blocks = Bitmap(width, maxY + 2)
-        for (line in lines) {
-            line.splitToSequence(" -> ").map {
-                val (x, y) = it.split(",", limit = 2)
-                x.toInt() to y.toInt()
-            }.zipWithNext { (x1, y1), (x2, y2) ->
-                for (x in minOf(x1, x2)..maxOf(x1, x2)) {
-                    for (y in minOf(y1, y2)..maxOf(y1, y2)) {
-                        blocks[x, y] = true
+        val width = maxOf(maxX, 500 + maxY + 1) + 1
+        val blocks = BooleanArray(width * (maxY + 2))
+        for (segments in points) {
+            segments.reduce { point1, point2 ->
+                for (x in minOf(point1.first, point2.first)..maxOf(point1.first, point2.first)) {
+                    for (y in minOf(point1.second, point2.second)..maxOf(point1.second, point2.second)) {
+                        blocks[x + y * width] = true
                     }
                 }
-            }.count()
-        }
-    }
-
-    @Day.Part
-    fun part1(): Int {
-        val blocks = Bitmap(blocks)
-        return generateSequence { fall(blocks, maxY) }
-            .takeWhile { it.second < maxY }
-            .onEach { blocks[it.first, it.second] = true }
-            .count()
-    }
-
-    @Day.Part
-    fun part2(): Int {
-        val blocks = Bitmap(blocks)
-        return generateSequence { fall(blocks, maxY + 1) }
-            .onEach { blocks[it.first, it.second] = true }
-            .indexOf(500 to 0) + 1
-    }
-
-    private class Bitmap private constructor(private val data: BooleanArray, private val width: Int) {
-        constructor(width: Int, height: Int) : this(BooleanArray(width * height), width)
-        constructor(other: Bitmap) : this(other.data.copyOf(), other.width)
-
-        operator fun get(x: Int, y: Int): Boolean {
-            require(x in 0 until width)
-            return data[x + y * width]
-        }
-
-        operator fun set(x: Int, y: Int, value: Boolean) {
-            require(x in 0 until width)
-            data[x + y * width] = value
-        }
-    }
-
-    companion object {
-        private fun fall(blocks: Bitmap, maxY: Int): IntPair {
-            var x = 500
-            for (y in 1..maxY) {
-                when {
-                    !blocks[x, y] -> {}
-                    !blocks[x - 1, y] -> x--
-                    !blocks[x + 1, y] -> x++
-                    else -> return x to y - 1
-                }
+                point2
             }
-            return x to maxY
         }
+        var count = 0
+        var countAtMaxY = -1
+        DeepRecursiveFunction<IntPair, Boolean> { (x, y) ->
+            check(x in 0 until width)
+            if (blocks[x + y * width]) return@DeepRecursiveFunction true
+            if (y == maxY && countAtMaxY < 0) countAtMaxY = count
+            val shouldFill = y > maxY ||
+                callRecursive(x to y + 1) && callRecursive(x - 1 to y + 1) && callRecursive(x + 1 to y + 1)
+            if (shouldFill) {
+                blocks[x + y * width] = true
+                count++
+            }
+            shouldFill
+        }(500 to 0)
+        check(countAtMaxY >= 0)
+        countAtMaxY to count
     }
+
+    @Day.Part
+    fun part1(): Int = result.first
+
+    @Day.Part
+    fun part2(): Int = result.second
 }
