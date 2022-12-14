8504f1513ed398140b2aef298946541a77522284
Daniel Lin <ephemient@gmail.com>
2022-12-14 05:55:12 -0500

    Speed up with array instead of set

--- hs/src/Day14.hs
+++ hs/src/Day14.hs
@@ -6,48 +6,60 @@ Description:    <https://adventofcode.com/2022/day/14 Day 14: Regolith Reservoir
 module Day14 (day14a, day14b) where
 
 import Common (readEntire)
-import Control.Monad ((>=>))
-import Data.List (foldl', unfoldr)
-import Data.Semigroup (Max(Max))
-import Data.Set (Set)
-import qualified Data.Set as Set (empty, insert, fromList, member, notMember, toList)
+import Control.Monad (forM_)
+import Data.Array.IArray (IArray, Ix, (!), (//))
+import Data.Array.MArray (newArray, writeArray)
+import Data.Array.ST (runSTUArray)
+import Data.Array.Unboxed (UArray)
+import Data.List (unfoldr)
+import Data.Semigroup (Max(Max), Min(Min))
 import Data.Text (Text)
 import qualified Data.Text as T (lines, splitOn)
 import qualified Data.Text.Read as T (decimal)
 
-parse :: (Integral a) => Text -> Set (a, a)
-parse = Set.fromList . (T.lines >=> parseLine) where
-    parseLine line = concat $ zipWith segment points (tail points) where
-        points =
-          [ (x', y')
-          | [x, y] <- T.splitOn "," <$> T.splitOn " -> " line
-          , x' <- either fail pure $ readEntire T.decimal x
-          , y' <- either fail pure $ readEntire T.decimal y
-          ]
-        segment (x1, y1) (x2, y2)
-          | x1 == x2 = [(x1, y) | y <- [min y1 y2..max y1 y2]]
-          | y1 == y2 = [(x, y1) | x <- [min x1 x2..max x1 x2]]
-          | otherwise = []
+parse :: (Bounded i, Integral i, Ix i) => Text -> (UArray (i, i) Bool, i)
+parse input = (blocks, maxY) where
+    segments =
+      [ [ (x', y')
+        | [x, y] <- T.splitOn "," <$> T.splitOn " -> " line
+        , x' <- either fail pure $ readEntire T.decimal x
+        , y' <- either fail pure $ readEntire T.decimal y
+        ]
+      | line <- T.lines input
+      ]
+    f (x, y) = (Min x, Max x, Min y, Max y)
+    (Min minX, Max maxX, Min minY, Max maxY) = foldMap f $ concat segments
+    blocks = runSTUArray $ do
+        a <- newArray
+          ( (min minX $ 500 - maxY - 1, min 0 minY)
+          , (max maxX $ 500 + maxY + 1, max 0 maxY + 1)
+          ) False
+        forM_ segments $ \points ->
+            forM_ (zip points $ tail points) $ \((x1, y1), (x2, y2)) ->
+                forM_ [min y1 y2..max y1 y2] $ \y ->
+                    forM_ [min x1 x2..max x1 x2] $ \x ->
+                        writeArray a (x, y) True
+        pure a
 
-fall :: (Num a, Ord a) => a -> Set (a, a) -> Maybe (Either a (a, a))
+fall :: (IArray a Bool, Ix i, Num i) => i -> a (i, i) Bool -> Maybe (Either i (i, i))
 fall maxY blocks
-  | (500, 0) `Set.member` blocks = Nothing
+  | blocks ! (500, 0) = Nothing
   | otherwise = pure $ fall' (500, 0) where
     fall' (x, y)
-      | y > maxY = Left x
-      | (x, y + 1) `Set.notMember` blocks = fall' (x, y + 1)
-      | (x - 1, y + 1) `Set.notMember` blocks = fall' (x - 1, y + 1)
-      | (x + 1, y + 1) `Set.notMember` blocks = fall' (x + 1, y + 1)
+      | y >= maxY = Left x
+      | not $ blocks ! (x, y + 1) = fall' (x, y + 1)
+      | not $ blocks ! (x - 1, y + 1) = fall' (x - 1, y + 1)
+      | not $ blocks ! (x + 1, y + 1) = fall' (x + 1, y + 1)
       | otherwise = Right (x, y)
 
 day14a :: Text -> Int
 day14a input = length $ unfoldr f blocks where
-    blocks = parse @Int input
-    Max maxY = foldMap (Max . snd) blocks
-    f blocks' = fall maxY blocks' >>= either (const Nothing) (Just . ((),) . (`Set.insert` blocks'))
+    (blocks, maxY) = parse @Int input
+    f blocks' = fall maxY blocks' >>= either (const Nothing) (g blocks')
+    g blocks' point = Just ((), blocks' // [(point, True)])
 
 day14b :: Text -> Int
 day14b input = length $ unfoldr f blocks where
-    blocks = parse @Int input
-    Max maxY = succ <$> foldMap (Max . snd) blocks
-    f blocks' = ((),) . (`Set.insert` blocks') . either (, maxY) id <$> fall maxY blocks'
+    (blocks, maxY) = parse @Int input
+    f blocks' = g blocks' . either (, maxY + 1) id <$> fall (maxY + 1) blocks'
+    g blocks' point = ((), blocks' // [(point, True)])
