4126166626b7b7267a87668a3aa43345c96bf644
Daniel Lin <ephemient@gmail.com>
2022-12-18 05:49:22 -0500

    Simplify

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day17.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2022/Day17.kt
@@ -11,114 +11,78 @@ class Day17(lines: List<String>) {
     fun part2(): Long = solve(1000000000000)
 
     private fun solve(n: Long): Long {
-        val buffer = mutableListOf<State>()
-        val cycle = generateSequence(Triple(State(0, byteArrayOf(127)), 0, 0)) { (state, jetIndex, rockIndex) ->
-            var i = jetIndex
-            val rock = rocks[rockIndex]
-            val maxX = 7 - rock.maxOf { it.length }
+        var jetIndex = 0
+        var state = State(0, byteArrayOf(127))
+        val seen = mutableMapOf<Triple<Int, Int, List<Byte>>, Long>()
+        val heights = mutableListOf<Long>()
+        for (i in 0 until n) {
+            val rockIndex = (i % rocks.size).toIntExact()
+            seen.put(Triple(rockIndex, jetIndex, state.rows.asList()), i)?.let { j ->
+                val q = (n - j) / (i - j)
+                val r = (n - j) % (i - j)
+                return heights[(j + r).toIntExact()] + q * (state.height - heights[j.toIntExact()])
+            }
+            val (width, rock) = rocks[rockIndex]
             var x = 2
-            var y = state.height + 3
-            while (true) {
-                val x2 = when (jet[i]) {
-                    '<' -> (x - 1).coerceAtLeast(0)
-                    '>' -> (x + 1).coerceAtMost(maxX)
-                    else -> throw IllegalArgumentException("${jet.take(i)}[${jet[i]}${jet.drop(i + 1)}")
+            var y = state.rows.size + 3
+            while (!state.contains(x, y, rock)) {
+                val x2 = when (val c = jet[jetIndex]) {
+                    '<' -> x - 1
+                    '>' -> x + 1
+                    else -> throw IllegalArgumentException("${jet.take(jetIndex)}$c${jet.drop(jetIndex + 1)}")
                 }
-                i = (i + 1) % jet.length
-                x = if (state.contains(x2, y, rock)) x else x2
-                if (state.contains(x, y - 1, rock)) break
+                jetIndex = (jetIndex + 1) % jet.length
+                x = if (x2 in 0..7 - width && !state.contains(x2, y, rock)) x2 else x
                 y--
             }
-            Triple(state.plus(x, y, rock), i, (rockIndex + 1) % rocks.size)
-        }.onEach { buffer.add(it.first) }.map { (state, jetIndex, rockIndex) ->
-            Triple(
-                state.rows.asList(),
-                jetIndex,
-                rockIndex
-            )
-        }.take((n + 1).coerceAtMost(Int.MAX_VALUE.toLong()).toInt()).asIterable().findCycle()
-        return if (cycle != null) {
-            val height1 = buffer[cycle.first].height
-            val height2 = buffer[cycle.second].height
-            val dividend = n - cycle.first
-            val divisor = cycle.second - cycle.first
-            val quotient = dividend / divisor
-            val remainder = dividend % divisor
-            buffer[cycle.first + remainder.toIntExact()].height + (height2 - height1) * quotient
-        } else {
-            buffer[n.toIntExact()].height.toLong()
+            heights.add(state.height)
+            state = state.plus(x, y + 1, rock)
         }
+        return state.height
     }
 
-    private class State(height: Int, val rows: ByteArray) {
-        private val yOffset = height - rows.size
-        val height: Int get() = yOffset + rows.size
-
-        @Suppress("ReturnCount")
-        fun contains(x: Int, y: Int, rock: Array<String>): Boolean {
-            require(y >= yOffset) { "$yOffset/$y/$height" }
-            rock.forEachIndexed { dy, line ->
-                val row = rows.getOrElse(y - yOffset + dy) { return false }
-                line.forEachIndexed { dx, c -> if (c == '#' && row.toInt() and 1.shl(x + dx) != 0) return true }
+    private class State(val height: Long, val rows: ByteArray) {
+        fun contains(x: Int, y: Int, rock: ByteArray): Boolean {
+            require(y >= 0) { "$y/${rows.size}" }
+            return rock.withIndex().any { (dy, row) ->
+                rows.getOrElse(y + dy) { return false }.toInt() and row.toInt().shl(x) != 0
             }
-            return false
         }
 
         @Suppress("CyclomaticComplexMethod")
-        fun plus(x: Int, y: Int, rock: Array<String>): State {
-            require(y >= yOffset)
-            val newHeight = maxOf(height, y + rock.size)
-            val rows = rows.copyOf(newHeight - yOffset)
-            rock.forEachIndexed { dy, line ->
-                var row = rows[y - yOffset + dy]
-                line.forEachIndexed { dx, c -> if (c == '#') row = (row.toInt() or 1.shl(x + dx)).toByte() }
-                rows[y - yOffset + dy] = row
-            }
+        fun plus(x: Int, y: Int, rock: ByteArray): State {
+            val rows = rows.copyOf(maxOf(rows.size, y + rock.size))
+            val height = height - this.rows.size + rows.size
+            rock.forEachIndexed { dy, row -> rows[y + dy] = (rows[y + dy].toInt() or row.toInt().shl(x)).toByte() }
             val visible = ByteArray(rows.size + 1).apply { set(lastIndex, 1) }
             val queue = mutableListOf(visible.lastIndex shl 3)
             while (queue.isNotEmpty()) {
                 val pos = queue.removeLast()
                 if (pos < visible.lastIndex shl 3 && rows[pos shr 3].toInt() and 1.shl(pos and 7) != 0) continue
-                if (pos > 7 && visible[pos - 8 shr 3].toInt() and 1.shl(pos and 7) == 0) {
-                    visible[pos - 8 shr 3] = (visible[pos - 8 shr 3].toInt() or 1.shl(pos and 7)).toByte()
-                    queue.add(pos - 8)
-                }
-                if (pos and 7 != 0 && visible[pos shr 3].toInt() and 1.shl(pos - 1 and 7) == 0) {
-                    visible[pos shr 3] = (visible[pos shr 3].toInt() or 1.shl(pos - 1 and 7)).toByte()
-                    queue.add(pos - 1)
-                }
-                if (pos and 7 < 6 && visible[pos shr 3].toInt() and 1.shl(pos + 1 and 7) == 0) {
-                    visible[pos shr 3] = (visible[pos shr 3].toInt() or 1.shl(pos + 1 and 7)).toByte()
-                    queue.add(pos + 1)
-                }
-                if (pos < visible.lastIndex shl 3 && visible[pos + 8 shr 3].toInt() and 1.shl(pos and 7) == 0) {
-                    visible[pos + 8 shr 3] = (visible[pos + 8 shr 3].toInt() or 1.shl(pos and 7)).toByte()
-                    queue.add(pos + 8)
+                for (next in intArrayOf(pos - 8, pos - 1, pos + 1, pos + 8)) {
+                    if (
+                        next and 7 != 7 && next in 0 until visible.size.shl(3) &&
+                        visible[next shr 3].toInt() and 1.shl(next and 7) == 0
+                    ) {
+                        visible[next shr 3] = (visible[next shr 3].toInt() or 1.shl(next and 7)).toByte()
+                        queue.add(next)
+                    }
                 }
             }
             rows.forEachIndexed { i, row -> rows[i] = (row.toInt() and visible[i].toInt()).toByte() }
             val trim = rows.indexOfFirst { it != 0.toByte() }
-            return State(newHeight, if (trim > 0) rows.copyOfRange(fromIndex = trim, toIndex = rows.size) else rows)
+            return State(height, if (trim > 0) rows.copyOfRange(fromIndex = trim, toIndex = rows.size) else rows)
         }
     }
 }
 
 private val rocks = arrayOf(
-    arrayOf("####"),
-    arrayOf(".#.", "###", ".#."),
-    arrayOf("###", "..#", "..#"),
-    arrayOf("#", "#", "#", "#"),
-    arrayOf("##", "##")
+    IndexedValue(4, byteArrayOf(15)),
+    IndexedValue(3, byteArrayOf(2, 7, 2)),
+    IndexedValue(3, byteArrayOf(7, 4, 4)),
+    IndexedValue(1, byteArrayOf(1, 1, 1, 1)),
+    IndexedValue(2, byteArrayOf(3, 3)),
 )
-
-private fun <T> Iterable<T>.findCycle(): IntPair? {
-    val indices = mutableMapOf<T, Int>()
-    forEachIndexed { j, value ->
-        val i = indices.getOrPut(value) { j }
-        if (i < j) return i to j
-    }
-    return null
-}
 
 private fun Long.toIntExact(): Int {
     require(this in Int.MIN_VALUE.toLong()..Int.MAX_VALUE.toLong())
