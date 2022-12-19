1610569a181d4356648f8a8838b0b0f0d2d3538c
Daniel Lin <ephemient@gmail.com>
2022-12-19 11:01:27 -0500

    Brute-force DFS in parallel

--- hs/src/Day19.hs
+++ hs/src/Day19.hs
@@ -2,9 +2,11 @@
 Module:         Day19
 Description:    <https://adventofcode.com/2022/day/19 Day 19: Not Enough Minerals>
 -}
-{-# LANGUAGE OverloadedStrings, TypeFamilies, ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 module Day19 (day19a) where
 
+import Control.Arrow (second)
+import Control.Parallel.Strategies (parMap, rseq)
 import Data.Char (isAlphaNum)
 import qualified Data.Heap as Heap (FstMaxPolicy, insert, singleton, view)
 import Data.List (foldl', scanl', transpose)
@@ -28,31 +30,10 @@ parser = Map.fromList <$> many blueprint where
     ore = flip (,) <$> L.decimal <*> (char ' ' >> name)
     name = takeWhile1P Nothing isAlphaNum
 
-search :: (Ord a, Ord b) => (a -> (Maybe b, [(b, a)])) -> (b, a) -> [a]
-search next = search' Nothing . Heap.singleton @Heap.FstMaxPolicy where
-    search' bestEstimate (Heap.view -> Just ((b, a), heap))
-      | fromMaybe False $ (<) <$> potential <*> bestEstimate = search' bestEstimate heap
-      | otherwise = a : search' (Just $ maybe b (max b) bestEstimate) heap' where
-            (potential, nexts) = next a
-            heap' = foldl' (flip Heap.insert) heap nexts
-    search' _ _ = []
-
 geodes :: (IsString k, Ord k, Num v, Ord v) => Int -> Map k (Map k v) -> v
-geodes n blueprint = foldl' max' 0 $ search next (0, (initialRobots, initialResources, n, [])) where
+geodes n blueprint = go 0 (initialRobots, initialResources, n, []) where
     initialRobots = Map.insert "ore" 1 $ const 0 <$> blueprint
     initialResources = const 0 <$> blueprint
-    next (robots, resources, m, log) = (Just $ potential robots resources m, options) where
-        estimate = resources Map.! "geode" + fromIntegral m * robots Map.! "geode"
-        options =
-          [ (estimate', (robots', resources'', m - d - 1, (n - m + d, robot):log))
-          | (robot, costs) <- Map.toList blueprint
-          , (d, resources') <- take 1 $ dropWhile (any (< 0) . snd) $
-                zip [0..m - 1] $ iterate (Map.unionWith (+) robots) $
-                Map.unionWith (+) resources $ negate <$> costs
-          , let estimate' = if robot == "geode" then estimate + fromIntegral (d - 1) else estimate
-                robots' = Map.insertWith (+) robot 1 robots
-                resources'' = Map.unionWith (+) robots resources'
-          ]
     potential robots resources m = potentialResources !! m Map.! "geode" where
         potentialRobots = Map.fromDistinctAscList . zip (Map.keys blueprint) <$> transpose
           [ (robots Map.! robot +) <$> scanl' f 0 potentialResources
@@ -63,10 +44,22 @@ geodes n blueprint = foldl' max' 0 $ search next (0, (initialRobots, initialReso
                   | otherwise = new
           ]
         potentialResources = scanl' (Map.unionWith (+)) resources potentialRobots
-    max' prev (robots, resources, m, log) =
-        max prev $ resources Map.! "geode" + fromIntegral m * robots Map.! "geode"
+    go k (_, _, 0, log) = k
+    go k (robots, resources, m, log)
+      | k > potential robots resources m = k
+      | otherwise = foldl' go
+          (max k $ resources Map.! "geode" + fromIntegral m * robots Map.! "geode")
+          [ (robots', resources'', m - d - 1, (n - m + d, robot):log)
+          | (robot, costs) <- Map.toList blueprint
+          , (d, resources') <- take 1 $ dropWhile (any (< 0) . snd) $
+                zip [0..m - 1] $ iterate (Map.unionWith (+) robots) $
+                Map.unionWith (+) resources $ negate <$> costs
+          , let robots' = Map.insertWith (+) robot 1 robots
+                resources'' = Map.unionWith (+) robots resources'
+          ]
 
 day19a :: Text -> Either (ParseErrorBundle Text Void) Int
 day19a input = do
     blueprints <- parse (parser @Int @Int @Void <* eof) "day19.txt" input
-    pure $ sum $ uncurry (*) <$> traceShowId [(i, geodes 24 blueprint) | (i, blueprint) <- Map.toList blueprints]
+    pure . sum . fmap (uncurry (*)) .
+        parMap rseq (traceShowId . second (geodes 24)) $ Map.toList blueprints
