4c44cbddaadaf53a831c8747f4b405b203476fbd
Daniel Lin <ephemient@gmail.com>
2022-12-21 05:28:31 -0500

    Simplify

--- hs/src/Day21.hs
+++ hs/src/Day21.hs
@@ -6,7 +6,7 @@ Description:    <https://adventofcode.com/2022/day/21 Day 21: Monkey Math>
 module Day21 (day21a, day21b) where
 
 import Data.Char (isAlphaNum)
-import qualified Data.Map as Map ((!), delete, fromList)
+import qualified Data.Map as Map ((!), fromList, insert)
 import Data.Ratio ((%), denominator, numerator)
 import Data.String (IsString)
 import Data.Text (Text)
@@ -15,21 +15,16 @@ import Text.Megaparsec (MonadParsec, ParseErrorBundle, Token, Tokens, (<|>), cho
 import Text.Megaparsec.Char (eol, string)
 import qualified Text.Megaparsec.Char.Lexer as L (decimal)
 
-data Linear a = Linear !a !a deriving (Eq, Ord, Show)
-data Expr a b = Literal a | Reference b | Expr a b :+ Expr a b | Expr a b :- Expr a b | Expr a b :* Expr a b | Expr a b :/ Expr a b deriving (Show)
+data Expr a b = Literal a | b :+ b | b :- b | b :* b | b :/ b
 
 parser :: (Integral a, MonadParsec e s m, IsString (Tokens s), Token s ~ Char) => m [(Tokens s, Expr a (Tokens s))]
 parser = parseLine `sepEndBy` eol where
     parseLine = (,) <$> name <* string ": " <*> expr
-    expr = (Literal <$> L.decimal) <|> do
-        lhs <- name
-        op <-
-            (:+) <$ string " + " <|>
-            (:-) <$ string " - " <|>
-            (:*) <$ string " * " <|>
-            (:/) <$ string " / "
-        rhs <- name
-        pure $ Reference lhs `op` Reference rhs
+    expr = Literal <$> L.decimal <|> flip ($) <$> name <*> op <*> name
+    op = (:+) <$ string " + " <|>
+        (:-) <$ string " - " <|>
+        (:*) <$ string " * " <|>
+        (:/) <$ string " / "
     name = takeWhile1P Nothing isAlphaNum
 
 day21a :: Text -> Either (ParseErrorBundle Text Void) Int
@@ -37,31 +32,32 @@ day21a input = do
     monkeys <- Map.fromList <$> parse parser "day21.txt" input
     let monkeys' = eval <$> monkeys
         eval (Literal a) = a
-        eval (Reference b) = monkeys' Map.! b
-        eval (a :+ b) = eval a + eval b
-        eval (a :- b) = eval a - eval b
-        eval (a :* b) = eval a * eval b
-        eval (a :/ b) = eval a `div` eval b
+        eval (x :+ y) = monkeys' Map.! x + monkeys' Map.! y
+        eval (x :- y) = monkeys' Map.! x - monkeys' Map.! y
+        eval (x :* y) = monkeys' Map.! x * monkeys' Map.! y
+        eval (x :/ y) = monkeys' Map.! x `div` monkeys' Map.! y
     pure $ monkeys' Map.! "root"
 
 day21b :: Text -> Either (ParseErrorBundle Text Void) Int
 day21b input = do
     monkeys <- Map.fromList <$> parse parser "day21.txt" input
-    let Reference lhs :+ Reference rhs = monkeys Map.! "root"
-        monkeys' = eval <$> Map.delete "root" monkeys
+    let (lhs, rhs) = case monkeys Map.! "root" of
+            lhs :+ rhs -> (lhs, rhs)
+            lhs :- rhs -> (lhs, rhs)
+            lhs :* rhs -> (lhs, rhs)
+            lhs :/ rhs -> (lhs, rhs)
+        monkeys' = Map.insert "humn" (1, 0) $ eval <$> monkeys
         eval (Literal a) = (0, a % 1)
-        eval (Reference "humn") = (1, 0)
-        eval (Reference b) = monkeys' Map.! b
-        eval (a :+ b) = eval a +: eval b
-        eval (a :- b) = eval a -: eval b
-        eval (a :* b) = eval a *: eval b
-        eval (a :/ b) = eval a /: eval b
+        eval (x :+ y) = (monkeys' Map.! x) +: (monkeys' Map.! y)
+        eval (x :- y) = (monkeys' Map.! x) -: (monkeys' Map.! y)
+        eval (x :* y) = (monkeys' Map.! x) *: (monkeys' Map.! y)
+        eval (x :/ y) = (monkeys' Map.! x) /: (monkeys' Map.! y)
         (a, b) +: (c, d) = (a + c, b + d)
         (a, b) -: (c, d) = (a - c, b - d)
         (0, b) *: (c, d) = (b * c, b * d)
         (a, b) *: (0, d) = (a * d, b * d)
         (a, b) /: (0, d) = (a / d, b / d)
-        (m, c) = monkeys' Map.! lhs
-        (n, d) = monkeys' Map.! rhs
-        x = (d - c) / (m - n)
+        (m, b) = monkeys' Map.! lhs
+        (n, c) = monkeys' Map.! rhs
+        x = (c - b) / (m - n)
     pure $ if denominator x == 1 then numerator x else error "non-integral"
